b83db57d6e2298e542874e4b236334e9340961fc
Benjamin S <cph-bs190@cphbusiness.dk>
2020-09-03 17:53:28 +0200

    should be complete

--- 3 - Wednesday/http_protocol/src/main/java/servlets/RequestInfo.java
+++ 3 - Wednesday/http_protocol/src/main/java/servlets/RequestInfo.java
@@ -35,7 +35,7 @@ public class RequestInfo extends HttpServlet {
         
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("Headers<hr/>");
+        out.println("Request Headers<hr/>");
         Enumeration<String> headerNames = request.getHeaderNames();
         while (headerNames.hasMoreElements()) {
                 String headerName = headerNames.nextElement();
